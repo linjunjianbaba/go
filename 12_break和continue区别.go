ccc452a06f439cc190717c6df986dbf4ec0339b8
bill <18898879769@163.com>
2019-03-09 17:47:56 +0800

    gogo

---
+++ 12_break和continue区别.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	i := 0
+	for { //for后面不写任何东西，这个循环条件永远为真，死循环
+		i++
+		time.Sleep(time.Second) //演示1s
+		if i == 5 {
+			//break //跳出循环，如果嵌套多个循环，跳出最近的那个内循环
+			continue //跳出本次循环，下一次继续
+		}
+		fmt.Println("i = ", i)
+
+	}
+}
---
+++ 13_goto的使用.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//goto可以用在任何地方，但是不能跨函数使用
+	fmt.Println("1111111111")
+
+	goto Biao //goto关键字，Biao用户起的名字，也可以叫标签
+
+	fmt.Println("2222222222")
+Biao:
+	fmt.Println("标标标标标标")
+}
